b18718bf931179b51e8fc5fd8f89bdac8ff43c1d
najeebkp <56462749+najeebkp@users.noreply.github.com>
2019-10-12 11:33:07 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 LOGIN_REDIRECT_URL = '/' # It means home view
 APPEND_SLASH = True
