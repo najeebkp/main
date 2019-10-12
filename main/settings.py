e8c53602984ee9b5114c8c37fc985ed34bc445fb
H4CK3R <najeebthangalkp@gmail.com>
2019-10-12 19:33:15 +0530

    initial commit

--- main/settings.py
+++ main/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
 }
 import dj_database_url
 db_from_env =dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default']=dj_database_url.config(default=os.environ['DATABASE_URL'])
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
