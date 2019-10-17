b01beae3622b10800a059340db3487763530dd10
H4CK3R <najeebthangalkp@gmail.com>
2019-10-17 19:59:03 +0530

    Initial

--- main/settings.py
+++ main/settings.py
@@ -146,11 +146,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static","staticfiles"),
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static")
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root","static")
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
