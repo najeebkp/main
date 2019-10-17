c49f5512ff1178b196c3240e386c27c73f1fac2a
H4CK3R <najeebthangalkp@gmail.com>
2019-10-17 19:36:46 +0530

    Initial

--- main/settings.py
+++ main/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
